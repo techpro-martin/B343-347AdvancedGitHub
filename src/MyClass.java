bc55d46d59f2a8bc077329715af63194e8298d4a
techpro-martin <martin.techproed@gmail.com>
2025-06-23 21:04:24 +0300

    v3

--- src/MyClass.java
+++ src/MyClass.java
@@ -5,4 +5,7 @@ public class MyClass {
 
     //Version v2
     public String string2 = "Bu da baska bir string'dir.";
+
+    //Version v3
+    public String string3 = "Bu da cok daha baska bir string'dir.";
 }
