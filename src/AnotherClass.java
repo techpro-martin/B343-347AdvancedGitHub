8973cdd3f5c509d229d36b2949db7d875f10974c
techpro-martin <martin.techproed@gmail.com>
2025-07-07 22:27:09 +0300

    t8

--- src/AnotherClass.java
+++ src/AnotherClass.java
@@ -1,4 +1,7 @@
 public class AnotherClass {
     //Techpro branch, version t7
     public String string1 = "Biri sen, biri ben, iki damla yas akti gozlerimden";
+
+    //Techpro branch, t8
+    public String string2 = "Parildayip duran insanlara bak";
 }
