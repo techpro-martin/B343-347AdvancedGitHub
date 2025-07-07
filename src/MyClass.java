40732b3b5e86fa26c5d82cf5096bb23245b02a3e
techpro-martin <martin.techproed@gmail.com>
2025-07-07 21:26:04 +0300

    t6

--- src/MyClass.java
+++ src/MyClass.java
@@ -29,4 +29,7 @@ public class MyClass {
 
     //Techpro branch, version t5
     public String string10 = "Affet beni aksam ustu";
+
+    //Conflict example, t6
+    public String string11 = "Bu aksam olurum kimse beni tutamaz";
 }
